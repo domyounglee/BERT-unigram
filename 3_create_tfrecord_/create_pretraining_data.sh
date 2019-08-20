8ec8bae8f011d33119ede43aa5931cd2b7acb2e7
domyounglee <zorbaalive@gmail.com>
2019-08-20 15:05:56 +0900

    asdf

---
+++ 3_create_tfrecord_/create_pretraining_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BERT_BASE_DIR="/root/workspace"
+INPUT_FILES="/root/workspace/train_corpus/512/total*"
+export CUDA_VISIBLE_DEVICES=-1
+FILENAME=`echo "$file" | cut -d "." -f1 | cut -d "/" -f6`
+
+echo ${FILENAME}
+python create_pretraining_data.py \
+    --input_file=$INPUT_FILES \
+	--output_file=$BERT_BASE_DIR/bert_corpus/${FILENAME}_512.tfrecord \
+	--vocab_file=$BERT_BASE_DIR/2_unigram_result/final_total_vocab.txt \
+	--do_lower_case=False \
+	--max_seq_length=512 \
+	--max_predictions_per_seq=20 \
+	--masked_lm_prob=0.15 \
+    --do_whole_word_mask=True \
+    --random_seed=12345 \
+	--dupe_factor=5 
+
